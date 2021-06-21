07bf9c3d9ec8340979679bf7b2bbb21ed8d561b1
njawandhia <64945898+njawandhia@users.noreply.github.com>
2021-06-21 08:09:07 -0500

    Add files via upload

---
+++ send_sms.py
@@ -0,0 +1,44 @@
+# Download the helper library from https://www.twilio.com/docs/python/install
+import os
+from twilio.rest import Client
+
+
+# Find your Account SID and Auth Token at twilio.com/console
+# and set the environment variables. See http://twil.io/secure
+account_sid = os.environ['ACce13f1271d48b79ee5f1be6237878aca']
+auth_token = os.environ['e82aad157cefd1be62df1c74e87cc14e']
+client = Client(account_sid, auth_token)
+
+message = client.messages \
+                .create(
+                     body="Hi Raphael! This is Nidhi Jawandhia from CPITS",
+                     from_='+18016917702',
+                     to='+19722663942'
+                 )
+
+print(message.sid)
+
+{
+  "account_sid": "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  "api_version": "2010-04-01",
+  "body": "Join Earth's mightiest heroes. Like Kevin Bacon.",
+  "date_created": "Thu, 30 Jul 2015 20:12:31 +0000",
+  "date_sent": "Thu, 30 Jul 2015 20:12:33 +0000",
+  "date_updated": "Thu, 30 Jul 2015 20:12:33 +0000",
+  "direction": "outbound-api",
+  "error_code": null,
+  "error_message": null,
+  "from": "+15017122661",
+  "messaging_service_sid": "MGXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  "num_media": "0",
+  "num_segments": "1",
+  "price": null,
+  "price_unit": null,
+  "sid": "SMXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  "status": "sent",
+  "subresource_uris": {
+    "media": "/2010-04-01/Accounts/ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Messages/SMXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Media.json"
+  },
+  "to": "+15558675310",
+  "uri": "/2010-04-01/Accounts/ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Messages/SMXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX.json"
+}
